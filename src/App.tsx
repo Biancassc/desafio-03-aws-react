1487bcb3c7e7fc4fb51e03066632f2108e8eb33d
biancassc <biancassc26@gmail.com>
2024-11-08 14:41:28 -0300

    feat: config Outlete router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-
-
 import './App.css'
-
+import { Outlet} from 'react-router-dom';
 function App() {
-  
-
-  return (
-      <div>
+ return(
+      <div className='App'>
+        <h1></h1>
+        <Outlet />
         </div>
-  )
+       );
 }
+export default App;
 
-export default App
