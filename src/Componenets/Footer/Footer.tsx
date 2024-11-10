34dcd7ba5fd3fd1610bff46c43abc47d3ceb6895
biancassc <biancassc26@gmail.com>
2024-11-09 23:08:31 -0300

    style: add footer css and import to footer.tsx

--- src/Componenets/Footer/Footer.tsx
+++ src/Componenets/Footer/Footer.tsx
@@ -4,10 +4,11 @@ import YouTubeIcon from "../../Assets/Social Icons.svg"
 import FacebookIcon from "../../Assets/Social Icons (1).svg"
 import TwitterIcon from "../../Assets/Frame 126.svg"
 import BrazilIcon from "../../Assets/Vector (2).svg"
+import "../../Componenets/Footer/Footer.css"
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Assim que possível, me envie um email para que possamos trabalhar felizes juntos!</p>
+      <p className='happy-work'>Assim que possível, me envie um email para que possamos trabalhar felizes juntos!</p>
       <div className="social-icons">
       <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
       <img src={InstagramIcon} alt="Instagram" width="40" height="40" />
@@ -22,12 +23,14 @@ const Footer = () => {
           <img src={YouTubeIcon} alt="YouTube" width="40" height="40" />
         </a>
       </div>
+      <div className='Copy-location'>
       <div className="location">
       <img src={BrazilIcon} alt="Brasil" />
         <p>Brasil</p>
       </div>
       <div className="copyright">
         <p>&copy; 2024, All Rights Reserved by Compass UOL</p>
+      </div>
       </div>
     </footer>
   );
