5e2d796bb29ca649a0f1bae2db75f10535da91fb
biancassc <biancassc26@gmail.com>
2024-11-09 21:30:23 -0300

    feat: implement logatou and githum login functionality

--- src/Componenets/Header/Header.tsx
+++ src/Componenets/Header/Header.tsx
@@ -1,20 +1,51 @@
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
+import "./Header.css";
+import { MdExitToApp } from 'react-icons/md';
+import { auth, githubProvider } from "../../firebase-config";
+import { signInWithPopup } from 'firebase/auth';
 
-
-const Header = () => {
+interface HeaderProps {
+    isProfileEdit?: boolean; 
+  }
+  
+  const Header = ({ isProfileEdit }: HeaderProps) => {
+    const navigate = useNavigate();
+  
+    // Função de Logout
+    const handleLogout = () => {
+     
+      console.log('Logout');
+      navigate('/');  
+    };
+  
+   
+    const handleGitHubLogin = async () => {
+      try {
+        const result = await signInWithPopup(auth, githubProvider);
+        const user = result.user;
+        console.log('GitHub login successful:', user);
+        navigate('/profileedit', { state: { login: user.displayName, avatar_url: user.photoURL } });
+      } catch (error) {
+        console.error("Error during GitHub login:", error);
+      }
+    };
   return (
-    <header className="header">
+    <header className="header" >
       <nav>
         <ul>
-          <li><Link to="/">Início</Link></li>
+        <li><Link to="/">Início</Link></li>
           <li><Link to="/minha-historia">Minha História</Link></li>
           <li><Link to="/experiencias">Experiências</Link></li>
           <li><Link to="/contato">Contato</Link></li>
-          <li><Link to="/entrar">Entrar</Link></li>
+          {isProfileEdit ? (
+            <li><button onClick={handleLogout}><MdExitToApp /> Sair</button></li>
+          ) : (
+            <li><button onClick={handleGitHubLogin}>Entrar</button></li>
+          )}
         </ul>
       </nav>
     </header>
   );
-};
+  }
 
 export default Header;
