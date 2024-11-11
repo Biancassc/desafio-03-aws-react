f6beeac22aebd5a6798ed35dc7cfed58f25877ef
biancassc <biancassc26@gmail.com>
2024-11-11 10:28:58 -0300

    refactor: reorganize return for bette readability from css

--- src/Routes/Profile/Profile.tsx
+++ src/Routes/Profile/Profile.tsx
@@ -35,70 +35,77 @@ const Profile = () => {
     <div>
       <Header isProfileEdit={false} />
       <div className="profile-container">
-        {user ? (
-          <div className="profile-content">
-            <div className="profile-header">
-              <div className="profile-photo">
-                <img
-                  src={user.avatar_url}
-                  alt={user.login}
-                  className="profile-avatar"
-                />
-              </div>
-
-              <div className="user-info">
-                <h1 className="greeting">Hello, I'm {userName}</h1>
-                <h2>{user.login}</h2>
-                {user.location && (
-                  <p>
-                    <span>{user.location}</span>
-                  </p>
-                )}
-                <h3>{user.email}</h3>
-              </div>
-            </div>
-
-            <div className="bio">
-              <span>{user.bio}</span>
-            </div>
-
-            <div className="social-links">
-              <a
-                href={`https://github.com/${user.login}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button>GitHub</button>
-              </a>
-              <a
-                href={`https://www.linkedin.com/in/${user.login}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button>LinkedIn</button>
-              </a>
-              <History
-                isEditing={false}
-                history={history}
-                onHistoryChange={(e) => setHistory(e.target.value)}
-              />
-              <div className="experiences-section">
-                {experiences.length === 0 ? (
-                  <div className="Noting">
-                    <h1>Experiências</h1>
-                  <p className="Noting">Não há nada por aqui!</p>
-                  </div>
-                ) : (
-                  <Experiences experiences={experiences} />
-                )}
-                <Email isEditing={false} email={email} />
-              </div>
-            </div>
-          </div>
-        ) : (
-          <p>Usuário não encontrado!</p>
-        )}
+  {user ? (
+    <div className="profile-content">
+      <div className="left-info"> 
+        <div className="profile-photo">
+          <img
+            src={user.avatar_url}
+            alt={user.login}
+            className="profile-avatar"
+          />
+        </div>
+        <div className="user-info">
+          <h2>{user.login}</h2>
+          {user.location && (
+            <p>
+              <span>{user.location}</span>
+            </p>
+          )}
+          <h3>{user.email}</h3>
+        </div>
       </div>
+
+      <div className="right-info">
+  <h1 className="greeting">
+    Hello, I'm <span className="username">{userName}</span>
+  </h1>
+        <div className="bio">
+          <span>{user.bio}</span>
+        </div>
+        <div className="social-links">
+          <a
+            href={`https://github.com/${user.login}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="github-button">GitHub</button>
+          </a>
+          <a
+            href={`https://www.linkedin.com/in/${user.login}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="linkedin-button">LinkedIn</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <p>Usuário não encontrado!</p>
+  )}
+</div>
+
+
+      <div className="experiences-and-history">
+        <History
+          isEditing={false}
+          history={history}
+          onHistoryChange={(e) => setHistory(e.target.value)}
+        />
+        <div className="experiences-section">
+          {experiences.length === 0 ? (
+            <div className="Noting">
+              <h1>Experiências</h1>
+              <p className="Noting">Não há nada por aqui!</p>
+            </div>
+          ) : (
+            <Experiences experiences={experiences} />
+          )}
+        </div>
+        <Email isEditing={false} email={email} />
+      </div>
+
       <Footer />
     </div>
   );
